a95402aed5c826003c8d36f716ba1f4a174f5958
Nikhil Warke <nikhil.warke@artofliving.org>
2023-03-27 07:27:54 +0530

    update url

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import { type Category, type FromAccount } from './domain'
 
 const url =
-  'http://161.35.218.30:10000/api/webhooks/trigger/app_e5d5106edf334b0a90cf997ebc8a2a7d/wh_36be15efd19d49f68ab84bec55199d1e'
+  'http://161.35.218.30:10000/api/webhooks/trigger/app_e5d5106edf334b0a90cf997ebc8a2a7d/wh_5eda2dd78f8a4313955cfd590eb2013a'
 
 export interface Payload {
   categories?: Category[]
