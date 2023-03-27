748b9302a862fca12cae8a8b35ea8aa7d29ddc27
Nikhil Warke <nikhil.warke@artofliving.org>
2023-03-27 18:56:46 +0530

    test: Add html test reporter

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  test: {
+    reporters: ['html']
+  },
   build: {
     target: 'esnext',
     sourcemap: true
